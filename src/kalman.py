6bacc1a1d54752644fbcaa062d7cbc9836d5fc8f
eamontracey <etracey@nd.edu>
2024-02-18 02:57:10 -0500

    update kalman filtration and sensor shenanigans

--- src/kalman.py
+++ src/kalman.py
@@ -4,35 +4,27 @@ from filterpy.kalman import KalmanFilter
 import numpy as np
 
 
-class Kalman:
+class DataFilter:
     def __init__(self):
+        self._dt = 0
+        self._t_prev = None
+        self._initialize_filter()
+        
         self.kalman_altitude = 0
         self.kalman_velocity = 0
         self.kalman_acceleration = 0
-        self.orientation_beta = 0
-        self._dt = 0
-        self._t_prev = None
-
-        self._initialize_filter()
 
     def _initialize_filter(self):
-        # Initializing what sensor data is being read
         sensor_matrix = [
             [1, 0, 0],
             [0, 0, 1]
-            # [0, 0, 1]
         ]
 
-        # Initializing Kalman Filter function
         self.filter = KalmanFilter(dim_x=3, dim_z=len(sensor_matrix))
         self.filter.H = np.array(sensor_matrix)
 
-        # Covariance (error in estimate)
         self.filter.P *= 1
-
-        # Measurement Noise
         self.filter.R *= 1
-
         self.filter.Q *= 1
         self.filter.x = np.array([0, 0, 0])
 
@@ -44,11 +36,14 @@ class Kalman:
         self._t_prev = in_time
 
     def _generate_phi(self):
-        self._calculate_dt(time.time())
+        if self.spoof is not None:
+            self._calculate_dt(next(self.time_gen))
+        else:
+            self._calculate_dt(time.time())
 
         dp = 1
-        ds = 0  # filler
-        di = (self._dt ** 2) / 2  # differences in time from current to previous state
+        ds = 0
+        di = (self._dt ** 2) / 2
 
         phi = np.array([
             [dp, self._dt, di],
@@ -58,13 +53,8 @@ class Kalman:
 
         return phi
 
-    def filter_data(self, altitude, acceleration_acce_z, linacceleration_imu_z, eulerangle_imu_z):
-        # Read sensor data
-        measurements = [
-            float(altitude),
-            float(acceleration_acce_z - 9.80665)
-            # float(linacceleration_imu_z - 9.80665)
-        ]
+    def filter_data(self, altitude, acceleration):
+        measurements = [float(altitude), float(acceleration)]
 
         params = np.array(measurements)
         self.filter.F = self._generate_phi()
@@ -73,4 +63,3 @@ class Kalman:
         self.filter.update(params)
 
         self.kalman_altitude, self.kalman_velocity, self.kalman_acceleration = self.filter.x
-        self.orientation_beta = eulerangle_imu_z
--- src/main.py
+++ src/main.py
@@ -4,141 +4,84 @@ import csv
 from datetime import datetime
 import time
 
-time.sleep(3)
-
-#import kalman
 import sensors
 from state import State
 
+# Initialize data logging constants.
 now = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-OUTPUT_DIR = "/home/acs/data/subscale"
-OUTPUT_PATH = f"{OUTPUT_DIR}/ACTUAL_SUBSCALE_2_data_{now}.csv"
-
+OUTPUT_DIR = "/home/acs/data/fullscale"
+OUTPUT_PATH = f"{OUTPUT_DIR}/fullscale_data_{now}.csv"
 HEADERS = [
     "Time",
     "State",
-#    "Temperature BMP",
-    "Pressure",
+    "Altitude Filtered",
+    "Acceleration Filtered",
+    "Velocity Filtered",
     "Altitude",
     "Acceleration X",
     "Acceleration Y",
     "Acceleration Z",
-    "Gravity X",
-    "Gravity Y",
-    "Gravity Z",
-    "Gyro X",
-    "Gyro Y",
-    "Gyro Z",
-    "Magnetic X",
-    "Magnetic Y",
-    "Magnetic Z",
-    "Euler X",
-    "Euler Y",
-    "Euler Z",
-    "Quaternion X",
-    "Quaternion Y",
-    "Quaternion Z",
-    "Quaternion W",
-    "Linear Acceleration X",
-    "Linear Acceleration Y",
-    "Linear Acceleration Z",
-    "Temperature BNO"
+    "Euler Angle 0",
+    "Euler Angle 1",
+    "Euler Angle 2",
+    "Temperature"
 ]
 
+# Create csv writer (logger).
 writer = csv.writer(open(OUTPUT_PATH, "w"))
 writer.writerow(HEADERS)
 
-#try:
-#    sensors.ALTIMETER.zero()
-#except Exception as e:
-#    print(f"{e}: failed to zero BMP390.")
+# Make the data filter.
+data_filter = DataFilter()
 
-#state = State.GROUND
+# Zero the altimeter.
+try:
+    sensors.ALTIMETER.zero()
+except Exception as e:
+    print(f"{e}: failed to zero BMP390.")
 
+# Begin the control loop.
 start = time.time()
 while True:
     try:
         # Read current time.
         current = time.time() - start
 
-#        # Read BMP390 sensor.
-#        temperature_bmp = sensors.ALTIMETER.temperature()
-        # Read MPRLS sensor.
-        try:
-            pressure = sensors.ALTIMETER.pressure()
-        except Exception as e:
-            pressure = "FAIL"
-            print(e)
+         # Read BMP390 sensor.
+        altitude = sensors.AlTIMETER.altitude()
+        temperature = sensors.ALTIMETER.temperature()
 
-        altitude = sensors.AlTIMETER.pressure()
-        state = state(altitude, acceleration_y)
         # Read BNO055 sensor.
         acceleration = sensors.IMU.acceleration()
         acceleration_x = acceleration[0]
         acceleration_y = acceleration[1]
         acceleration_z = acceleration[2]
-        gravity = sensors.IMU.gravity()
-        gravity_x = gravity[0]
-        gravity_y = gravity[1]
-        gravity_z = gravity[2]
-        gyro = sensors.IMU.gyro()
-        gyro_x = gyro[0]
-        gyro_y = gyro[1]
-        gyro_z = gyro[2]
-        magnetic = sensors.IMU.magnetic()
-        magnetic_x = magnetic[0]
-        magnetic_y = magnetic[1]
-        magnetic_z = magnetic[2]
-        euler = sensors.IMU.euler()
-        euler_x = euler[0]
-        euler_y = euler[1]
-        euler_z = euler[2]
-        quaternion = sensors.IMU.quaternion()
-        quaternion_x = quaternion[0]
-        quaternion_y = quaternion[1]
-        quaternion_z = quaternion[2]
-        quaternion_w = quaternion[3]
-        linear_acceleration = sensors.IMU.linear_acceleration()
-        linear_acceleration_x = linear_acceleration[0]
-        linear_acceleration_y = linear_acceleration[1]
-        linear_acceleration_z = linear_acceleration[2]
-        temperature_bno = sensors.IMU.temperature()
+        state = state_determination(altitude, acceleration_y)
+
+        # Filter the data.
+        data_filter.filter_data(altitude, acceleration_y)
+        altitude_filtered = data_filter.kalman_altitude
+        acceleration_filtered = data_filter.kalman_acceleration
+        velocity_filtered = data_filter.kalman_velocity
+
+        print(altitude_filtered)
+
+
+        # Log the data
         writer.writerow([
-            current,
+            current
             state,
-#            temperature_bmp,
-            pressure,
+            altitude_filtered,
+            acceleration_filtered,
+            velocity_filtered,
             altitude,
             acceleration_x,
             acceleration_y,
             acceleration_z,
-            gravity_x,
-            gravity_y,
-            gravity_z,
-            gyro_x,
-            gyro_y,
-            gyro_z,
-            magnetic_x,
-            magnetic_y,
-            magnetic_z,
-            euler_x,
-            euler_y,
-            euler_z,
-            quaternion_x,
-            quaternion_y,
-            quaternion_z,
-            quaternion_w,
-            linear_acceleration_x,
-            linear_acceleration_y,
-            linear_acceleration_z,
-            temperature_bno
+            euler_angle_0,
+            euler_angle_1,
+            euler_angle_2,
+            temperature
         ])
     except Exception as e:
         print(e)
-    if state == 'BURNOUT':
-        rotate_servo(20)
-        sleep(3)
-        rotate_servo(0)
-        break
-
-
