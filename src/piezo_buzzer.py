b46ce1a37ff15b3465019f3cbe2cacc6dfcd510f
eamontracey <etracey@nd.edu>
2024-02-22 00:40:25 +0000

    teasley

--- src/piezo_buzzer.py
+++ src/piezo_buzzer.py
@@ -1,23 +1,18 @@
 import time
 
+import board
 import pwmio
 
 
-class Buzzer:
+class PiezoBuzzer:
     def __init__(self, pin, **kwargs):
         self.buzzer = pwmio.PWMOut(pin, **kwargs)
-        self._frequency = 500
 
-    @property
-    def frequency(self):
-        return self._frequency
-
-    @frequency.setter
-    def frequency(self, f):
-        self._frequency = f
-        self.buzzer.frequency = f
-
-    def beep(self, s, duty_cycle=2 ** 15):
+    def beep(self, s, frequency=500, duty_cycle=2 ** 15):
+        self.buzzer.frequency = frequency
         self.buzzer.duty_cycle = duty_cycle
         time.sleep(s)
         self.buzzer.duty_cycle = 0
+
+
+BUZZER = PiezoBuzzer(board.D13)
