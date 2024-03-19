291e7072e13ca742db52273b2a34b7dcdbc2b411
eamontracey <etracey@nd.edu>
2024-03-18 20:45:55 -0400

    begin the process of python packaging

--- lib/control/actuation_controller.py
+++ lib/control/actuation_controller.py
@@ -4,11 +4,38 @@ import time
 
 from state import State
 
-# Assume servo actuation percentage = flap angle.
-class ActuationController:
-    def __init__(self, servo):
-        self.servo = servo
+def calculate_drag(self, flap_angle, velocity):
+    # This is a rough calculation of mach number given velocity.
+    # Improvement: calculate based on temperature.
+    mach_number = velocity / 1125 
 
+    # This equation is based on interpolated CFD results.
+    # Output is newtons.
+    return (
+        -20.74
+        + 4.351 * flap_angle
+        + 131.1 * mach_number
+        - 0.1112 * flap_angle**2
+        - 19.77 * flap_angle * mach_number
+        + 146 * mach_number**2
+        + 0.5031 * flap_angle**2 * mach_number
+        + 31.64 * flap_angle * mach_number ** 2
+        + 117.8 * mach_number ** 3
+    )
+
+def predict_apogee(self, altitude, acceleration, velocity, drag):
+    radicand = VEHICLE_MASS * G / drag
+    if radicand < 0:
+        return 5769
+    velocity_terminal = velocity * math.sqrt(radicand)
+
+    apogee_delta = velocity_terminal ** 2 * math.log(1 + velocity ** 2 / velocity_terminal ** 2) / (2 * G)
+    apogee_prediction = altitude + apogee_delta
+
+    return apogee_prediction
+
+class ActuationController:
+    def __init__(self):
         self.error_previous = 0
         self.time_previous = 0
         self.integral_previous = 0
@@ -20,56 +47,15 @@ class ActuationController:
         # We want to log this.
         self.apogee_prediction = 5800
 
-        self.servo.rotate(0)
-        time.sleep(2)
-        self.servo.rotate(30)
-        time.sleep(2)
-        self.servo.rotate(0)
+    def determine_actuation(self, state, altitude, acceleration, velocity, time_):
+        # Returns the value to pass to servo.rotate.
 
-    def calculate_drag(self, velocity):
-        # Assume flap angle equals servo actuation percentage.
-        # This is not perfectly accurate but a decent approximation.
-        flap_angle = self.servo.percentage
-
-        # This is a rough calculation of mach number given velocity.
-        # Improvement: calculate based on temperature.
-        mach_number = velocity / 1125 
-
-        # This equation is based on interpolated CFD results.
-        # Output is newtons.
-        return (
-            -20.74
-            + 4.351 * flap_angle
-            + 131.1 * mach_number
-            - 0.1112 * flap_angle**2
-            - 19.77 * flap_angle * mach_number
-            + 146 * mach_number**2
-            + 0.5031 * flap_angle**2 * mach_number
-            + 31.64 * flap_angle * mach_number ** 2
-            + 117.8 * mach_number ** 3
-        )
-        
-    def predict_apogee(self, altitude, acceleration, velocity, drag):
-        radicand = VEHICLE_MASS * G / drag
-        if radicand < 0:
-            logging.warning(f"radicand was 0 {radicand}")
-            return 5769
-        velocity_terminal = velocity * math.sqrt(radicand)
-
-        apogee_delta = velocity_terminal ** 2 * math.log(1 + velocity ** 2 / velocity_terminal ** 2) / (2 * G)
-        apogee_prediction = altitude + apogee_delta
-
-        return apogee_prediction
-
-    def actuate(self, state, altitude, acceleration, velocity, time_):
         if state == State.GROUND or state == State.LAUNCHED:
-            return
+            return None
         if state == State.OVERSHOOT:
-            self.servo.rotate(40)
-            return
+            return 40
         if state == State.APOGEE:
-            self.servo.rotate(0)
-            return
+            return 0
 
         if self._first:
             drag = self.calculate_drag(velocity)
@@ -82,7 +68,7 @@ class ActuationController:
             self.pi_previous = 0
 
             self._first = False
-            return
+            return None
 
         # Predict apogee.
         drag = self.calculate_drag(velocity)
@@ -105,10 +91,11 @@ class ActuationController:
 
         # Perform PI control!
         pi = dt * (Kp * proportional + Ki * integral) * Kg + self.pi_previous
-        self.servo.rotate(pi)
 
         # Store relevant previous values.
         self.error_previous = apogee_error
         self.time_previous = time_
         self.integral_previous = integral
         self.pi_previous = pi
+
+        return pi
---
+++ lib/control/constants.py
@@ -0,0 +1,15 @@
+# Physical constants.
+G = 32.17405                        # feet / second^2
+
+# Launch vehicle constants.
+VEHICLE_MASS = 1.1902231            # slugs
+
+# State determination constants.
+LAUNCH_ALTITUDE = 100               # feet
+LAUNCH_ACCELERATION = 200           # feet / second^2
+LAUNCH_ALTITUDE_CRITICAL = 300      # feet / second^2
+BURNOUT_ALTITUDE = 1000             # feet
+BURNOUT_ACCELERATION = 0            # feet / second^2
+BURNOUT_ALTITUDE_CRITICAL = 1500    # feet
+APOGEE_ALTITUDE = 5200              # feet
+APOGEE_VELOCITY = 0                 # feet
