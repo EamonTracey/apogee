f5181f1a07cb390a8ebfa07899fbc65665769fc0
eamontracey <etracey@nd.edu>
2024-03-19 22:27:28 -0400

    update burnout altitude cutoffs

--- src/acs2024/control.py
+++ src/acs2024/control.py
@@ -9,9 +9,9 @@ G = 32.17405                        # feet / second^2
 LAUNCH_ALTITUDE = 100               # feet
 LAUNCH_ACCELERATION = 200           # feet / second^2
 LAUNCH_ALTITUDE_CRITICAL = 300      # feet / second^2
-BURNOUT_ALTITUDE = 1000             # feet
+BURNOUT_ALTITUDE = 1200             # feet
 BURNOUT_ACCELERATION = 0            # feet / second^2
-BURNOUT_ALTITUDE_CRITICAL = 1500    # feet
+BURNOUT_ALTITUDE_CRITICAL = 1700    # feet
 APOGEE_ALTITUDE = 5200              # feet
 APOGEE_VELOCITY = 0                 # feet
 
